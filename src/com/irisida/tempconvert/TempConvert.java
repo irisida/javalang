2b7a548b8cb5112d11c61118201d709df028201d
irisida <20445648+irisida@users.noreply.github.com>
2020-12-19 23:44:59 +0000

    linting tidyup

--- src/com/irisida/tempconvert/TempConvert.java
+++ src/com/irisida/tempconvert/TempConvert.java
@@ -6,9 +6,8 @@ public class TempConvert {
     public static void main(String[] args) {
 
         /*
-         * create a scanner to accept user input. Also using printf to avoid a
-         * newline at the end of the prompt. Remembering to close the scanner
-         * and avoid leaks.
+         * create a scanner to accept user input. Also using printf to avoid a newline
+         * at the end of the prompt. Remembering to close the scanner and avoid leaks.
          */
         Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Enter a value in fahrenheit to convert to celsius >  ");
