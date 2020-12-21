aec3b800faa54ffff6313d7953625176ac7f6099
irisida <20445648+irisida@users.noreply.github.com>
2020-12-21 21:00:30 +0000

    added challenge of menu creation using classes and string arrays

---
+++ src/com/irisida/lang/projects/menuclassesproject/Menu.java
@@ -0,0 +1,19 @@
+package com.irisida.lang.projects.menuclassesproject;
+
+public class Menu {
+    String[] options;
+
+    void display() {
+        // display menu header
+        System.out.println("Select an option:");
+
+        // list the options provided as menu options
+        for (String option : this.options) {
+            System.out.printf("  %s%n", option);
+        }
+
+        // list the footer and selection prompt
+        System.out.printf("%n  Choice >> ");
+
+    }
+}
