451a67ad4f7681ebb0ea4d8e4f9f50a56d9ce159
irisida <20445648+irisida@users.noreply.github.com>
2020-12-21 16:37:50 +0000

    added multi-file example of class and package

---
+++ src/com/irisida/lang/methods/classfiles/CreatedObject.java
@@ -0,0 +1,9 @@
+package com.irisida.lang.methods.classfiles;
+
+/* create a simple class in this package but one that is located in
+ * its own file as per standard structure for larger programs that
+ * will be encountered in the real world.
+ */
+public class CreatedObject {
+    String name;
+}
