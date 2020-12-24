df3c859d172388a47ca0359f429aca6a8dcb6b75
irisida <20445648+irisida@users.noreply.github.com>
2020-12-24 12:49:30 +0000

    added a sample of static for creating static methods and using static counts to generate IDs

---
+++ src/com/irisida/lang/usingstatic/genids/Item.java
@@ -0,0 +1,32 @@
+package com.irisida.lang.usingstatic.genids;
+
+public class Item {
+    private int id;
+    private String name;
+    private static int count = 0;
+
+    public Item(String name) {
+        count++;
+        this.name = name;
+        this.id = count;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static int getCountOfItems() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Item [").append("\n");
+        sb.append("name = ").append(this.name);
+        sb.append("id = ").append(this.id);
+
+        return sb.toString();
+    }
+}
