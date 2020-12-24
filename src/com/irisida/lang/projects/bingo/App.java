c7d575d8afb81ac78284e411c69e76f5bccdca67
irisida <20445648+irisida@users.noreply.github.com>
2020-12-24 02:21:08 +0000

    added bingo simulator

---
+++ src/com/irisida/lang/projects/bingo/App.java
@@ -0,0 +1,10 @@
+package com.irisida.lang.projects.bingo;
+
+public class App {
+    public static void main(String[] args) {
+        Bingo bingo = new Bingo();
+
+        bingo.startGame();
+        bingo.endGame();
+    }
+}
