2ae652774a46350919fa22449b6514ac48973b2e
irisida <20445648+irisida@users.noreply.github.com>
2021-01-05 16:36:25 +0000

    remove serializable objects build warning by adding id

--- src/com/irisida/lang/part09/chapter23/serializingobjects/App.java
+++ src/com/irisida/lang/part09/chapter23/serializingobjects/App.java
@@ -8,6 +8,10 @@ import java.io.Serializable;
 import java.util.Objects;
 
 class Person implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     private String name;
     private int id;
 
